637871fff3fb3ac481104bdadd311805e6b469e0
Mello-Cello <celloelle@gmail.com>
2019-05-26 16:19:51 -0700

    Completed clock functionto display the time and update the time every 1 second. Nothing else is displayed on the page currently.

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-// Your code here
-let uts = new Date(Date.now());
-let hour = uts.getHours();
-let minutes = uts.getMinutes();
-let seconds = uts.getSeconds();
-hour = hour > 9 ? hour : '0' + hour;
-minutes = minutes > 9 ? minutes : '0' + minutes
-seconds = seconds > 9 ? seconds : '0' + seconds;
-// alternatively, add a 0 and then return the last two digits:
-// let hours = ('0'+ uts.getHours()).slice(-2);
-// let minutes = ('0'+ uts.getMinutes()).slice(-2);
-// let seconds = ('0'+ uts.getSeconds()).slice(-2);
 
-$(document).ready(function() {
-
-    let clockDisplay = $(`<span> ${hour}:${minutes}:${seconds} </span>` );
-    // let clockDisplay = $(`<span> ${uts} </span>`)
+$(document).ready(function () {
+    const clockRefreshId = window.setInterval (function() {
+    let uts = new Date(Date.now());
+    let hour = uts.getHours();
+    let minutes = uts.getMinutes();
+    let seconds = uts.getSeconds();
+    hour = hour > 9 ? hour : '0' + hour;
+    minutes = minutes > 9 ? minutes : '0' + minutes
+    seconds = seconds > 9 ? seconds : '0' + seconds;
+    // alternatively, add a 0 and then return the last two digits:
+    // let hours = ('0'+ uts.getHours()).slice(-2);
+    // let minutes = ('0'+ uts.getMinutes()).slice(-2);
+    // let seconds = ('0'+ uts.getSeconds()).slice(-2);
+    let currentTime = $(`<span> ${hour}:${minutes}:${seconds} </span>` );
     // let clockUts = $.now();
-    // console.log(clockUts);
-    $('#clock').html(clockDisplay);
-    
-
-    
+    $('#clock').html(currentTime);
+    // $('#clock').prepend(`current time`);
+    }, 1000);
 });
